407ac9054c1f758882fed7094894bec32842e4a9
Chidinma <10703081+chidinmae@users.noreply.github.com>
2026-05-30 16:07:33 +0200

    add cloudflare package back

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import wasm from 'vite-plugin-wasm'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
 
-// import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
-  server: {headers: {'Cross-Origin-Opener-Policy': 'same-origin', 'Cross-Origin-Embedder-Policy': 'require-corp'},},
+  plugins: [react(), wasm(), cloudflare()],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  }, build: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+  build: {
     sourcemap: false, // Ensures original TypeScript code cannot be reverse-engineered
-  }
-})
+  },
+});
